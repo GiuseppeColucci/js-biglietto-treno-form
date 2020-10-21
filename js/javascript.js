71b18b103390ecf4828a977f30bd73ccf16d1b34
giuseppecolucci <giuseppecolucciwork@gmail.com>
2020-10-21 17:21:55 +0200

    non funziona bene

--- js/javascript.js
+++ js/javascript.js
@@ -1,1 +1,44 @@
-console.log('works giuseppe')
+// PARTE ELEMENTI!!
+
+var container=document.getElementById('Biglietto');
+var bottoneGenera=document.getElementById('BottoneGenera');
+var bottoneAnnulla=document.getElementById('BottoneAnnulla');
+
+// generazione eventi!!
+
+bottoneGenera.addEventListener('click', function(){
+    // dati utente
+    var nome =document.getElementById('nome').value
+   
+    var kmDaPercorrere=document.getElementById('km').value
+    
+    var fasciaEta =document.getElementById('fascia-eta').value
+    
+    // costo BIGLIETTO
+    var prezzo=0.21;
+    var prezzoBiglietto=prezzo*kmDaPercorrere;
+    var offerta='biglietto Standard';
+
+    // condizzioni per l'offerta!!
+    // sconto minorenne
+    if(fasciaEta=='minorenne'){
+        prezzoBiglietto+=prezzoBiglietto*0.2;
+        offerta='sconto minorenne'
+    }
+    // sconto over65
+    else if(fasciaEta=='over65'){
+        prezzoBiglietto+=prezzoBiglietto*0.4;
+        offerta='sconto over65'
+    }
+    prezzoBiglietto=prezzoBiglietto.toFixed(2)+'Euro';
+    var NCarrozza=Math.floor(Math.random()*10)+1;
+    // codice CP------------------------------------->
+    document.getElementById('nome-passeggero').innerHTML=nome;
+    document.getElementById('offerta-applicata').innerHTML=offerta;
+    document.getElementById('carrozza').innerHTML=NCarrozza;
+    document.getElementById('costo').innerHTML=costo;
+
+
+
+})
+
